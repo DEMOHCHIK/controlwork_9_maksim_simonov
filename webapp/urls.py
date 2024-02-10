ee4f14c5b399ec3b9d054b5313c669899137309e
DEMOHCHIK <smnvmaks@gmail.com>
2024-02-10 13:58:42 +0600

    Реализовал создание и редактирование объявления

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from webapp.views import AdvertisementListView, AdvertisementDetailView
+from webapp.views import AdvertisementListView, AdvertisementDetailView, AdvertisementCreateView, \
+    AdvertisementUpdateView
 
 app_name = 'webapp'
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('', AdvertisementListView.as_view()),
     path('home/', AdvertisementListView.as_view(), name='index'),
     path('advertisement/<int:pk>/', AdvertisementDetailView.as_view(), name='advertisement_detail'),
+    path('advertisement/create/', AdvertisementCreateView.as_view(), name='create_advertisement'),
+    path('advertisement/<int:pk>/update/', AdvertisementUpdateView.as_view(), name='update_advertisement'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
+from webapp.forms import AdvertisementForm
 from webapp.models import Advertisement
 
 
@@ -17,3 +19,25 @@ class AdvertisementDetailView(DetailView):
     model = Advertisement
     template_name = 'advertisement/detail.html'
     context_object_name = 'advertisement'
+
+
+class AdvertisementCreateView(CreateView):
+    model = Advertisement
+    template_name = 'advertisement/create_advertisement.html'
+    form_class = AdvertisementForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('webapp:index')
+
+
+class AdvertisementUpdateView(UpdateView):
+    model = Advertisement
+    template_name = 'advertisement/edit_advertisement.html'
+    form_class = AdvertisementForm
+
+    def get_success_url(self):
+        return reverse_lazy('webapp:advertisement_detail', kwargs={'pk': self.object.pk})
